637c034c5df9a7344964d12f502e44a15619f881
小草莓 <zhangdi.vim@icloud.com>
2021-05-13 10:53:14 +0800

    panic和recover

---
+++ qimi/p11/panic_recover/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// p10 panic和recover
+
+// 注意：
+// 1. recover()必须搭配defer使用。
+// 2. defer一定要在可能引发panic的语句之前定义。
+
+func a() {
+	fmt.Println("func a")
+}
+
+func b() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("func b error")
+		}
+	}()
+	panic("panic in b")
+}
+
+func c() {
+	fmt.Println("func c")
+}
+
+func main() {
+	a()
+	b()
+	c()
+}
