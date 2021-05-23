6d4b1327314c8d28f476efff6a2fb8d475a24b6b
小草莓 <zhangdi.vim@icloud.com>
2021-05-23 23:27:08 +0800

    修改注释

--- oldboy/day04/08method_demo/main.go
+++ oldboy/day04/08method_demo/main.go
@@ -5,7 +5,6 @@ import "fmt"
 // 标识符：变量名，类型名，函数名，方法名
 // Go中如果标识符首字母是大写的，就表示对外可见（暴露的，公有的）
 
-// Go语言中的方法（Method）是一种作用于特定类型变量的函数。
 type dog struct {
 	name string
 }
@@ -17,7 +16,7 @@ func newDog(name string) dog {
 	}
 }
 
-// 方法是作用于特定类型的函数
+// Go语言中的方法（Method）是一种作用于特定类型变量的函数。
 // 接收者表示的是调用该方法的具体类型变量，多用类型名首字母小写表示
 func (d dog) wang() {
 	fmt.Printf("%s:汪汪汪~\n", d.name)
