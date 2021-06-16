389647826bc00baaf12bdc369001e43c6f130294
小草莓 <zhangdi.vim@icloud.com>
2021-06-17 00:03:28 +0800

    2

--- qimi/p2/const/main.go
+++ qimi/p2/const/main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
+// 常量
+// 定义的时候必须赋值
 const C = 3
 const E float32 = 2.71828
 const PI float64 = 3.14159
 
-// p3 常量
 func main() {
 	fmt.Println(C)
 	fmt.Println(E)
--- qimi/p2/iota/main.go
+++ qimi/p2/iota/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 /*
 iota是go语言的常量计数器，只能在常量的表达式中使用。
-
 iota在const关键字出现时将被重置为0。
 const中每新增一行常量声明将使iota计数一次(iota可理解为const语句块中的行索引)。
 使用iota能简化定义，在定义枚举时很有用。
@@ -30,7 +29,7 @@ const (
 	n12        // 3
 )
 
-// p3 常量
+// 常量之iota
 func main() {
 	fmt.Println(n1, n2, n3, n4)
 	fmt.Println(n5, n6, n8)
