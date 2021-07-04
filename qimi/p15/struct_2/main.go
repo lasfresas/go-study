76d11a27c8305a9b5ad9aa1e42ea41958c4b4fbb
小草莓 <zhangdi.vim@icloud.com>
2021-07-04 20:34:06 +0800

    15

--- qimi/p15/struct_2/main.go
+++ qimi/p15/struct_2/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 结构体的嵌套
 
-// 用type关键字造的,都是类型
+// 用type关键字造的，都是类型
 type Address struct {
 	Province string
 	City     string
@@ -14,7 +14,7 @@ type Person struct {
 	Name    string
 	Gender  string
 	Age     int
-	Address Address // 嵌套另外一个结构体。前面的是Person里面的字段名,后面的是指Address类型
+	Address Address // 嵌套另外一个结构体。前面的是Person里面的字段名，后面的是指Address类型
 }
 
 func main() {
@@ -24,7 +24,7 @@ func main() {
 		Age:    18,
 		Address: Address{
 			Province: "上海",
-			City:     "浦东新区",
+			City:     "浦东",
 		},
 	}
 	fmt.Printf("%#v\n", p1)
--- qimi/p15/struct_3/main.go
+++ qimi/p15/struct_3/main.go
@@ -4,19 +4,23 @@ import "fmt"
 
 // 结构体的继承
 
+// Animal 结构体
 type Animal struct {
 	name string
 }
 
+// Animal 的方法
 func (a *Animal) move() {
 	fmt.Printf("%s会动\n", a.name)
 }
 
+// Dog 结构体
 type Dog struct {
 	Feet    int
 	*Animal // 匿名嵌套，而且嵌套的是结构体指针
 }
 
+// Dog 的方法
 func (d *Dog) wang() {
 	fmt.Printf("%s会汪汪汪\n", d.name)
 }
